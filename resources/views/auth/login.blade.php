0a305be300f4eac25e5e025a7d70df288c18c822
thaidinh <thai562003@gmail.com>
2025-02-26 08:21:25 +0700

    Update Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,15 +15,15 @@
                 </div>
             </div>
 
-            <!-- Session Status -->
+            <!-- Trạng thái phiên -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
 
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <!-- Email Address -->
+                <!-- Địa chỉ Email -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
+                    <x-input-label for="email" :value="__('Địa chỉ Email')" />
                     <x-text-input id="email" class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
                                 type="email" 
                                 name="email" 
@@ -34,9 +34,9 @@
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
-                <!-- Password -->
+                <!-- Mật khẩu -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
+                    <x-input-label for="password" :value="__('Mật khẩu')" />
                     <x-text-input id="password" class="block mt-1 w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                 type="password"
                                 name="password"
@@ -45,13 +45,13 @@
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
-                <!-- Remember Me -->
+                <!-- Ghi nhớ đăng nhập -->
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
                         <input id="remember_me" type="checkbox" 
                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" 
                                name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Ghi nhớ đăng nhập') }}</span>
                     </label>
                 </div>
 
@@ -59,12 +59,12 @@
                     @if (Route::has('password.request'))
                         <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" 
                            href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
+                            {{ __('Quên mật khẩu?') }}
                         </a>
                     @endif
 
                     <x-primary-button class="ms-3">
-                        {{ __('Log in') }}
+                        {{ __('Đăng nhập') }}
                     </x-primary-button>
                 </div>
             </form>
